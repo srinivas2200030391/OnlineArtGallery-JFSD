6c3de2c7d8962a926cf79ddb85e9bdc6a8583e5a
Srinivas Kommirisetty <116977437+srinivas2200030391@users.noreply.github.com>
2024-12-09 10:04:30 +0530

    wtf is wrong with config.js

--- OnlineArtGalleryVite/src/config.js
+++ OnlineArtGalleryVite/src/config.js
@@ -1,6 +1,6 @@
 const config = {
   //baseURL: "http://localhost:1329",
-  baseURL:"https://online-art-gallery-backend-production.up.railway.app/"
+  baseURL:"https://online-art-gallery-backend-production.up.railway.app"
 };
 
 export default config;
