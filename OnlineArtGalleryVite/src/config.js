cc073faf485070ceda3b72015d82e7b21e128576
Srinivas Kommirisetty <116977437+srinivas2200030391@users.noreply.github.com>
2024-12-09 10:01:40 +0530

    Update config.js

--- OnlineArtGalleryVite/src/config.js
+++ OnlineArtGalleryVite/src/config.js
@@ -1,5 +1,6 @@
 const config = {
-  baseURL: "http://localhost:1329",
+  //baseURL: "http://localhost:1329",
+  baseURL:"https://online-art-gallery-backend-production.up.railway.app/"
 };
 
 export default config;
